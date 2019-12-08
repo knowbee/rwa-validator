2ef295d366a3a07e5b101cff6fa2f21fb964be84
Googler <knowbeeinc@gmail.com>
2019-12-08 11:22:51 +0200

    add more tests for ids

--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,19 @@ const { isNationalId, isPhoneNumber } = require("../index");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("rwa-validator", () => {});
+describe("rwa-validator", () => {
+  describe("validation national id numbers", () => {
+    it("should return true", () => {
+      expect(isNationalId("1199972222000040")).to.be.true;
+    });
+    it("should return false", () => {
+      expect(isNationalId("1189972222000040")).to.be.false;
+    });
+    it("should return false", () => {
+      expect(isNationalId("1219972222000040")).to.be.false;
+    });
+    it("should return true", () => {
+      expect(isNationalId("1199672222000040")).to.be.true;
+    });
+  });
+});
